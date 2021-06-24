307f30337e7822d5082265cb3fc0cecad1c2f3c6
nlfmt <nlfmt@gmx.de>
2021-06-24 09:41:48 +0200

    change

--- webapp/server.py
+++ webapp/server.py
@@ -39,6 +39,7 @@ def home():
     return render_template("home.html", x=x_axis, y=y_axis, max=daily_max, last=last_value, res=res)
 
 
+# Github Webhook
 @app.route("/reload_app", methods=["POST"])
 def reload_app():
     try:
