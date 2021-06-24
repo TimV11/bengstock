fad569633d86b251052ae2370fd4a0916d4525c3
nlfmt <nlfmt@gmx.de>
2021-06-24 11:04:18 +0200

    change

--- webapp/server.py
+++ webapp/server.py
@@ -42,31 +42,33 @@ def home():
 @app.route("/get_data")
 def send_data():
 
-    db.reconnect()
-    cur = db.cursor()
-    
-    cur.execute("select * from data")
-    res = cur.fetchall()
+    try:
+        db.reconnect()
+        cur = db.cursor()
+        
+        cur.execute("select * from data")
+        res = cur.fetchall()
 
-    format = "%Y-%m-%d %H.%M"
+        format = "%Y-%m-%d %H.%M"
 
-    x_axis = [datetime.strptime(f"{i[1]} {i[2]}", format).strftime("%H:%M") for i in res]
+        x_axis = [datetime.strptime(f"{i[1]} {i[2]}", format).strftime("%H:%M") for i in res]
 
-    y_axis = [i[3] / 100 for i in res]
+        y_axis = [i[3] / 100 for i in res]
 
-    daily_max = res[-1][4]
-    last_value = res[-1][3]
+        daily_max = res[-1][4]
+        last_value = res[-1][3]
 
-    data = {
-        "x": x_axis,
-        "y": y_axis,
-        "max": daily_max,
-        "last": last_value,
-        "res": res
-        }
+        data = {
+            "x": x_axis,
+            "y": y_axis,
+            "max": daily_max,
+            "last": last_value,
+            "res": res
+            }
 
-    return json.stringify(data), 200
-
+        return json.stringify(data), 200
+    except Exception as e:
+        return str(e), 200
 
 # GitHub Webhook to automatically pull code
 @app.route("/reload_app", methods=["POST"])
